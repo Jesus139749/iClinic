1bf5e4e98855ec9264e01b88e46628ef77929c0b
Jesus Miguel <jesusalmenares04@gmail.com>
2026-09-15 11:07:16 -0300

    feat: cria telas placeholder de Consultas, Favoritos e Perfil

---
+++ src/screens/Consultas/ConsultasScreen.tsx
@@ -0,0 +1,9 @@
+import {View,Text } from "react-native";
+
+export default function ConsultasScreen() {
+    return(
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-lg font-semibold text-gray-800">Minhas Consultas</Text>
+        </View>
+    );
+}
