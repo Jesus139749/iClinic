1981f836548b95393563a078d89a247d954bd7b4
Jesus Miguel <jesusalmenares04@gmail.com>
2026-09-15 11:54:03 -0300

    feat: configura navegação por tabs (Home, Consultas, Favoritos, Perfil)

---
+++ src/routes/tab.routes.tsx
@@ -0,0 +1,46 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import HomeScreen from '../screens/Home/HomeScreen';
+import LoginScreen from '../screens/Login/LoginScreen';
+import Consultas from '../screens/Consultas/ConsultasScreen';
+import Favoritos from '../screens/Favoritos/FavoritosScreen';
+import Perfil from '../screens/Perfil/PerfilScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabRoutes() {
+    return(
+        <Tab.Navigator screenOptions={{headerShown: false}}>
+            <Tab.Screen 
+            name="Home"
+            component={HomeScreen}
+            >
+            </Tab.Screen>
+
+            <Tab.Screen
+            name="Login"
+            component={LoginScreen}
+            >
+            </Tab.Screen>
+
+            <Tab.Screen
+            name="Consultas"
+            component={Consultas}
+            >   
+            </Tab.Screen>
+
+            <Tab.Screen
+            name="Favoritos"
+            component={Favoritos}
+            >
+            </Tab.Screen>
+
+            <Tab.Screen
+            name="Perfil"
+            component={Perfil}
+            >
+            </Tab.Screen>
+        </Tab.Navigator>
+
+    )
+}
