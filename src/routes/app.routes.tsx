49554ef90bf7134ad1ed3e7fc251717db1793f2e
Alysson <alyssonmths5@gmail.com>
2026-09-15 21:12:03 -0300

    feat(): tela de cadastro de clínica

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,16 +4,24 @@ import LoginScreen from '../screens/Login/LoginScreen';
 import CadastroPacienteScreen from '../screens/Paciente/Cadastro/CadastroPacienteScreen';
 import CadastroClinicaScreen from '../screens/Clinica/Cadastro/CadastroClinicaScreen';
 import TabRoutes from './tab.routes';
+import { Header } from '../components/Header';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppRoutes() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="CadastroPaciente" component={CadastroPacienteScreen} />
-      <Stack.Screen name="CadastroClinica" component={CadastroClinicaScreen} />
+      <Stack.Screen name="CadastroPaciente" component={CadastroPacienteScreen} options={{
+        headerShown: true,
+        header: () => <Header />
+      }} />
+      <Stack.Screen name="CadastroClinica" component={CadastroClinicaScreen}
+        options={{
+          headerShown: true,
+          header: () => <Header />
+        }} />
       <Stack.Screen name="Main" component={TabRoutes} />
     </Stack.Navigator>
   );
