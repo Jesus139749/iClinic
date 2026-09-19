12c6b9e06fde80586024f26802b719298f61e3f0
Alysson <alyssonmths5@gmail.com>
2026-09-18 23:11:35 -0300

    feat(): adicionado ícones na barra tab das telas da clínica

--- src/screens/Clinica/tabs/clinicatab.routes.tsx
+++ src/screens/Clinica/tabs/clinicatab.routes.tsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import DashboardClinicaScreen from '../Dashboard/DashboardClinicaScreen';
 import { MedicosScreen } from '../Medicos/MedicosScreen';
 import { AgendaScreen } from '../Agenda/AgendaScreen';
@@ -12,21 +13,57 @@ export default function ClinicaTabRoutes() {
             <Tab.Screen
                 name="Inicio"
                 component={DashboardClinicaScreen}
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons
+                            name={focused ? 'home' : 'home-outline'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
             />
 
             <Tab.Screen
                 name="Médicos"
                 component={MedicosScreen}
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons
+                            name={focused ? 'people' : 'people-outline'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
             />
 
             <Tab.Screen
                 name="Agenda"
                 component={AgendaScreen}
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons
+                            name={focused ? 'calendar' : 'calendar-outline'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
             />
 
             <Tab.Screen
                 name="Consultas"
                 component={ConsultasScreen}
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons
+                            name={focused ? 'clipboard' : 'clipboard-outline'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
             />
         </Tab.Navigator>
     );
